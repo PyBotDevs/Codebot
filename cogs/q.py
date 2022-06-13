af10e92f7fb4af9b52318a8d4009dfc4b9d2d13a
thatOneArchUser <69575053+thatOneArchUser@users.noreply.github.com>
2022-06-13 06:00:29 -0400

    Add files via upload

---
+++ cogs/q.py
@@ -0,0 +1,24 @@
+import discord, base64, brainfuck #pip install brainfuck-interpreter
+from discord.ext import commands
+
+class ex(commands.Cog):
+    def __init__(self, client:commands.Bot):
+        self.client = client
+
+    @commands.command(name="decode")
+    async def a(self, c, *, data):
+        if c == "bin" or c == "binary":
+            d = str()
+            for i in data.split():
+                a = int(i, 2)
+                d += chr(a)
+            return await ctx.reply(d)
+        elif c == "hex" or c == "hexadecimal":
+            return await ctx.reply(bytearray.fromhex(data).decode())
+        elif c == "base32":
+            return await ctx.reply(base64.b32decode(data).decode("UTF-8"))
+        elif c == "base64":
+            return await ctx.reply(base64.b64decode(data).decode("UTF-8"))
+        elif c == "bf" or c == "brainfk" or c == "brainfuck":
+            return await ctx.reply(brainfuck.evaluate(data))
+        else: raise BadArgument
